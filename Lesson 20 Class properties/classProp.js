d20a75f9e92660d249bcfae14ba1c098c1af6f8b
RamzesLys <ramzes311982@gmail.com>
2023-06-19 12:45:30 +0300

    Lesson 20 +

--- Lesson 20 Class properties/classProp.js
+++ Lesson 20 Class properties/classProp.js
@@ -1,37 +1,28 @@
+// class Counter {
+  
+//   constructor() {
+//     this.count = 0;
+//     this.increment = () => {
+//       this.count += Counter.incrementStep;
+//     }
+//   }
+// }
+
+// Counter.incrementStep = 2;
+
+// Counter.incrementAll = function(arr) {
+//   arr.forEach((c) = c.increment()) 
+// }
+
 class Counter {
   count = 0;
-  
-  constructor() {
-    this.count = 0;
-    this.increment = () => {
-      this.count += Counter.incrementStep;
-    }
+  increment = () => {
+    this.count += Counter.incrementStep;
   }
-  
- 
-//  inc = () => {
-//     this.count += Counter.incrementStep;
-//     console.log(this.count);
-//   }
-//   static incrementStep = 2;
-
-//   static incrementAll = function(arr) {
-//     arr.array.forEach((c) = c.inc()) 
-//   }
+  static incrementStep = 2;
+  static incrementAll = function (arr) {
+    arr.forEach((c) =>  c.increment());
+  }
 }
 
-Counter.incrementStep = 2;
-
-Counter.incrementAll = function(arr) {
-  arr.forEach((c) = c.increment()) 
-}
-
-
-// Counter.incrementAll
-
-// const cnt = new Counter();
-// console.log(cnt.count);
-
-// cnt.inc();
-// setTimeout(cnt.inc, 1000);
-
+console.log(Counter);
