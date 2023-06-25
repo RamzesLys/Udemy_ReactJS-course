fc15cfdbde33987b5e49d79285cbb1660d8b20af
RamzesLys <ramzes311982@gmail.com>
2023-06-25 11:44:57 +0300

    Lesson 35 event +

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,19 @@ import React , {Component} from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
+
+  //Old Method
+  // constructor () {
+  //   super();
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${this.props.label}`)
+  //   }
+  // }
+// NEW METHOD
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`)
+  }
+
   render () {
 
     const { label, important = false } = this.props;
@@ -14,7 +27,8 @@ export default class TodoListItem extends Component {
       <span className="todo-list-item">
         <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={this.onLabelClick}>
           {label}
         </span>
   
