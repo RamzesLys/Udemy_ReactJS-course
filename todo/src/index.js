23f6d8087946881ad0f3cc4a91848b72c4b34ea9
RamzesLys <ramzes311982@gmail.com>
2023-06-25 10:18:36 +0300

    Lesson 33 Structure +

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,35 +1,7 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import {createRoot} from 'react-dom/client';
-
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-import ItemStatusFilter from './components/item-status-filter';
-
-import './index.css';
+import App from './components/app'
 
 const root = createRoot(document.getElementById('root'));
-
-const App = () => {
-
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
-
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
 
 root.render(<App/>);
