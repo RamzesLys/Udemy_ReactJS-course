38c52a9953d7cbfaebfb2b2c8cf5832862b7b52f
RamzesLys <ramzes311982@gmail.com>
2023-06-25 11:09:33 +0300

    Lesson 34 Components-class +

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,32 +1,33 @@
-import React  from "react";
+import React , {Component} from "react";
 import "./todo-list-item.css";
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
+  render () {
 
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
-
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
+    const { label, important = false } = this.props;
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal'
+    };
+  
+    return (
+      <span className="todo-list-item">
+        <span
+          className="todo-list-item-label"
+          style={style}>
+          {label}
+        </span>
+  
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-end">
+          <i className="fa fa-exclamation" />
+        </button>
+  
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-end">
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-end">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-end">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
+    );
+  }
+}
