85fe00413589e01b004992ba1d58dabfa246853e
RamzesLys <ramzes311982@gmail.com>
2023-06-27 16:38:32 +0300

    Lesson 41 setState addElement +

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -3,10 +3,13 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
 export default class App extends Component {
+
+  maxId = 100;
 
   state = {
     todoData: [
@@ -19,8 +22,6 @@ export default class App extends Component {
   deleteItem = (id) => {
   this.setState(({ todoData}) => {
     const idx = todoData.findIndex((el) => el.id === id);
-
-
     const newArray = [
       ...todoData.slice(0, idx),
       ...todoData.slice(idx + 1)
@@ -31,6 +32,26 @@ export default class App extends Component {
     };
     });
   };
+
+  addItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    };
+
+    this.setState(({ todoData }) => {
+      
+      const newArr = [
+        ...todoData,
+        newItem
+      ]
+
+      return {
+        todoData: newArr
+      }
+    });
+  }
 
   render() {
     return (
@@ -44,6 +65,8 @@ export default class App extends Component {
         <TodoList 
         todos={this.state.todoData} 
         onDeleted = { this.deleteItem }/>
+
+        <ItemAddForm onItemAdded = {this.addItem}/>
       </div>
     );
   }
